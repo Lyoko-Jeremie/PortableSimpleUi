df573df82f5ae9895aae04877546fb8c978b88b2
LyokoJeremie <lucheng989898@gmail.com>
2026-06-02 16:36:47 +0800

    .

--- src/core.ts
+++ src/core.ts
@@ -108,11 +108,6 @@ export function createZoneWrapper(name: string): IZoneWrapper {
 }
 
 /**
- * initPortableSimpleUiZone 是 createZoneWrapper 的别名，用于向后兼容
- */
-export const initPortableSimpleUiZone = createZoneWrapper;
-
-/**
  * 包装 signal 以便在组件中使用
  */
 export interface ISignal<T> {
